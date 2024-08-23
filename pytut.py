eb3bee0e684bd14bf423a9d380530afb2aa28f09
Suda Srinivasan <suda.m.srinivasan@gmail.com>
2024-08-22 17:45:42 -0700

    Create pytut.py
    Python tutorial code in one file

---
+++ pytut.py
@@ -0,0 +1,14 @@
+# All code for learning will go into this file. I will comment out lines that I don't want to run
+
+# Print Hello World
+'''
+print("Hello world")
+'''
+
+# f-string
+
+name = "Suda"
+print(f"Hello, {name}")
+
+name = "Saat"
+print(f"Hello, {name}")
